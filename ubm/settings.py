c2a8d2947cca0ad1a646df4a455e115dc073eab3
Hari Prashant <60441372+hpb1@users.noreply.github.com>
2020-06-18 17:53:39 +0530

    Commit 2 - Update settings.py

--- ubm/settings.py
+++ ubm/settings.py
@@ -89,9 +89,6 @@ REST_FRAMEWORK = {
    'DEFAULT_AUTHENTICATION_CLASSES': (
        'rest_framework.authentication.TokenAuthentication',
    ),
-   'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAdminUser'
-   ),
 }
 
 # Password validation
